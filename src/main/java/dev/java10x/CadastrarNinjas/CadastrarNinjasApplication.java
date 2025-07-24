c6f78a77c4a533e1120e7df4e79897cacdd36eb9
Bruno Candido <bruno.georgecandido@gmail.com>
2025-07-24 15:54:35 -0300

    commit

---
+++ src/main/java/dev/java10x/CadastrarNinjas/CadastrarNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastrarNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrarNinjasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrarNinjasApplication.class, args);
+    }
+
+}
