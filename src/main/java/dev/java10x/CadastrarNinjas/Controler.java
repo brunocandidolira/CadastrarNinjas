d41343ac52972a8b866184a71dcb9941b526ff5d
Bruno Candido <bruno.georgecandido@gmail.com>
2025-07-24 22:44:45 -0300

    commit

---
+++ src/main/java/dev/java10x/CadastrarNinjas/Controler.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastrarNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controler {
+    @GetMapping("/testando")
+    public String testando() {
+        return "Testando a Rota";
+    }
+}
