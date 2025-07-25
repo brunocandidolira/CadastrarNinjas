1e6f2bf8507a9f0f2fe48bf5adee09fcd2cb6403
Bruno Candido <bruno.georgecandido@gmail.com>
2025-07-25 15:12:12 -0300

    commit

--- src/main/java/dev/java10x/CadastrarNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastrarNinjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controler {
+public class NinjaController {
     @GetMapping("/testando")
     public String testando() {
         return "Testando a Rota";
