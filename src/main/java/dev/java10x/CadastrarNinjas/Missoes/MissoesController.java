f8e1b9b07818daf9fc72ecd72863e14cf9b8e079
Bruno Candido <bruno.georgecandido@gmail.com>
2025-08-13 16:36:06 -0300

    feat: cria rotas do controller com mock

---
+++ src/main/java/dev/java10x/CadastrarNinjas/Missoes/MissoesController.java
@@ -0,0 +1,24 @@
+package dev.java10x.CadastrarNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+@PostMapping("/criar")
+    public String missoesCriar(){
+    return" missoes Criada" ;
+}
+@PutMapping ("/alterar")
+    public String missoesAlterar(){
+    return "alterado sua missao";
+}
+    @DeleteMapping("/deletar")
+    public String missoesDeletar(){
+        return "deletado sua missao";
+    }
+@GetMapping("/listar")
+public String listarMissoes(){
+    return "listar missoes com sucesso";
+}
+}
