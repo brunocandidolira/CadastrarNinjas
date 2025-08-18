c4d9b3e8631926db1d025a3b3ac00fc596a730b6
Bruno Candido <bruno.georgecandido@gmail.com>
2025-08-17 22:58:55 -0300

    adiciona uma nova rota de delecao do ninja

--- src/main/java/dev/java10x/CadastrarNinjas/Ninjas/Controller/Service/NinjaController.java
+++ src/main/java/dev/java10x/CadastrarNinjas/Ninjas/Controller/Service/NinjaController.java
@@ -15,29 +15,33 @@ public class NinjaController {
     }
 
     // Adicionar Ninja(CREATE)
-    @PostMapping ("/criar")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninja){
-        return  ninjaService.criarNinja(ninja);
+    @PostMapping("/criar")
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+        return ninjaService.criarNinja(ninja);
     }
 
     //Procurar Ninja por ID(CREATE)
     @GetMapping("/listar/{id}")
-    public NinjaModel listarNinjasPorId(@PathVariable long id){
+    public NinjaModel listarNinjasPorId(@PathVariable long id) {
         return ninjaService.listarNinjasPorId(id);
     }
+
     // mostrar ninjas todos os ninjas(READ)
     @GetMapping("/listar")
-    public List<NinjaModel> listarNinjas(){
-         return ninjaService.listarNinja();
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinja();
     }
+
     //alterar dados dos ninjas(UPDATE)
-     @PutMapping("/alterar")
-     public String aletrarNinjaPorId() {
-         return "alterar  ninja";
-     }
+    @PutMapping("/alterar")
+    public String aletrarNinjaPorId() {
+        return "alterar  ninja";
+    }
+
     //apagar ninjas(DELETE)
-         @DeleteMapping("/deletarId")
-         public String deletarNinjasPorId() {
-             return " ninja deletado";
-         }
+    @DeleteMapping("/deletar/{id}")
+    public void deletarNinjasPorId(@PathVariable Long id) {
+        ninjaService.deletarNinjaPorId(id);
+    }
 }
+
--- src/main/java/dev/java10x/CadastrarNinjas/Ninjas/Controller/Service/NinjaService.java
+++ src/main/java/dev/java10x/CadastrarNinjas/Ninjas/Controller/Service/NinjaService.java
@@ -28,4 +28,8 @@ public class NinjaService {
     public NinjaModel criarNinja(NinjaModel ninja) {
         return  ninjaRepository.save(ninja);
     }
+    // Deletar ninjas metodo void pos não precisa retornar um objeto
+    public void deletarNinjaPorId(Long id){
+        ninjaRepository.deleteById(id);
+    }
     }
